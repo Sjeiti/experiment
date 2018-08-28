b92474e9eb8c84e980abe27a1bee393bb4ab1bce
* <github@sjeiti.com>
2018-08-28 21:18:44 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//import asdd from './experiment'
+//alert(467)
+/*window.onerror = function(msg,uri) {
+    alert('Error caught'+msg+uri)
+}*/
+import xp from './experiment/river'
+//alert(!!xp)
+xp.init(document.body)
