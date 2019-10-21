27c3dc5c04bc2c92c540be71aa8886e518b0eb9e
Ron Valstar <github@sjeiti.com>
2019-10-21 21:56:57 +0200

    sort of final bezier

--- src/experiment/bezier.js
+++ src/experiment/bezier.js
@@ -16,7 +16,7 @@ let inst = experiment('plasma',{
   ,scale = 1
   ,center = vector(0,0)
   ,points = []
-  ,num = 120
+  ,num = 80
   ,elmCanvas
   ,context
   ,w,h
@@ -57,27 +57,57 @@ function handleAnimate(deltaT,millis) {
   context.closePath()
  
   context.fillStyle = strokeStyle
-  const f = 0.01
+  const f = 0.005
   const pn = (x,y)=>
     perlin.noise(f*x, f*y, 0.0005*millis)-0.5
 
   // zoom
   context.save()
-  if (false){
+  if (!false){
     const s = 1 + 2*(pn(262662,2770)+0.5)
     context.translate(w/2,h/2)
     context.scale(s,s)
     context.translate(-w/2,-h/2)
   }
 
-  const offset = 1444
+  const wSub = 1+1*3
+  const wP = w/(wSub-1)
+  const offset = 944
   const lineDist = h/num
+  const ln = []
   for(let i=0;i<num;i++){
     //const part = i/num
     //const part = (1+Math.cos(i/num*Math.PI))/2
     const part = 1 - Math.abs(i/num*2 - 1)
     const y = lineDist*i
     const lineH = (part+0.1)*lineDist * 0.7
+    for(let j=0;j<wSub;j++){
+      const x = j*wP
+      const k = 2*wSub-j-1
+      ln[j] = [x,y]
+      ln[k] = [x,y+lineH]
+      ln[j][1] += offset*pn(...ln[j])
+      ln[k][1] += offset*pn(...ln[k])
+    }
+    context.beginPath()
+    //ln.forEach((p,j)=>{
+    for(let j=0,l=ln.length;j<l;j++){
+      const p = ln[j]
+      if (j===0) context.moveTo(...p)
+      else if (j===wSub) context.lineTo(...p)
+      else {
+        const c1 = p//ln[j]
+        const c2 = ln[j+1]
+        const p3 = ln[j+2]
+        context.bezierCurveTo(...c1,...c2,...p3)
+        j += 2
+        //const q = 
+        //context.quadraticCurveTo(...q,...p)
+        //context.lineTo(...p)
+      }
+    }
+    //})
+    /*
     const p0 = [0,y]
     const p1 = [w/3,y]
     const p2 = [2*w/3,y]
@@ -96,11 +126,19 @@ function handleAnimate(deltaT,millis) {
     p7[1] += offset*pn(...p7)
     context.beginPath()
     context.moveTo(...p0)
-    context.bezierCurveTo(...p1,...p2,...p3)
+    //context.bezierCurveTo(...p1,...p2,...p3)
+    context.lineTo(...p1)
+    context.lineTo(...p2)
+    context.lineTo(...p3)
     context.lineTo(...p7)
-    context.bezierCurveTo(...p6,...p5,...p4)
+    context.lineTo(...p6)
+    context.lineTo(...p5)
+    context.lineTo(...p4)
+    //context.bezierCurveTo(...p6,...p5,...p4)
+    */
     context.fill()
     context.closePath()
+    //!window.p&&(alert(ln),window.p=true)
   }
 
   context.restore()
