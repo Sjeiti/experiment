ed1d4e18157e99ed44044f4117da43319e9e1d2f
Ron Valstar <ron@ronvalstar.nl>
2023-01-10 17:21:11 +0100

    fixed rotate experiment

--- src/experiment/rotate.js
+++ src/experiment/rotate.js
@@ -5,11 +5,9 @@
  */
 import experiment from './base'
 import {animate} from '../signal/signals'
-import {noise} from '../math/perlin'
-  
+
 let main
 let mainStyle
-let form
 
 function init(_target){
   initWrapper(_target)
@@ -18,19 +16,44 @@ function init(_target){
 }
 
 function initWrapper(root){
-  Array.from(new Array(22)).reduce((acc, o, i)=>{
-    const div = document.createElement('div')
-    i===0&&div.classList.add('rotate')
-    acc.appendChild(div)
-    return div
-  }, root)
 
-  run()
+  main = element('div', root,{class:'rotate'})
+  mainStyle = main.style
+  main.addEventListener('click', ()=>{
+    setCSSProp('c1', getRandomColor())
+    setCSSProp('c2', getRandomColor())
+  })
+
+  Array.from(new Array(22)).reduce(acc=>element('div', acc), main)
 }
 
 function initStyle(root){
   const style = document.createElement('style')
-  style.textContent = getStyle()
+  style.textContent = `
+    :root {
+      --t: 0;
+      --c1: black;
+      --c2: #F04;
+    }
+    
+    .rotate {
+      width: 100%;
+      height: 100%;
+      padding: min(3vw, 3vh);
+      background-size: 100% 100%;
+      background-image: repeating-linear-gradient(calc(-0.01deg*var(--t)), var(--c1) 0 4rem, var(--c2) 4rem 8rem);
+      overflow: hidden;
+    }
+    
+    .rotate div {
+      padding: inherit;
+      box-shadow: 0 0 2rem #00000044;
+      /*backdrop-filter: blur(0.2vw);*/
+      background-image: inherit;
+      transform: rotate(calc(0.001deg*var(--t)));
+      transform-origin: 50% 50%;
+      /*border-radius: 998rem;*/
+    }`
   root.appendChild(style)
 }
 
@@ -42,27 +65,11 @@ function exit(){
   animate.remove(onAnimate)
 }
 
-function onFormChange(e){
-  const {target: {name, value}} = e
-  setValue(name,value)
-  setData()
-}
-
-function onFormInput(e){
-  const {target: {name, value}} = e
-  setValue(name,value)
-}
-
 let start
 function onAnimate(deltaT,millis){
   start||(start=millis)
-  const speed = 0.0005
   const elapsed = millis-start
   setCSSProp('t', elapsed)
-  setCSSProp('sin', Math.sin(speed*elapsed))
-  setCSSProp('cos', Math.cos(speed*elapsed))
-  setCSSProp('x', noise(speed*elapsed,3876.13))
-  setCSSProp('y', noise(speed*elapsed,999.123))
 }
 
 function element(type, prnt, atts, evts) {
@@ -73,84 +80,14 @@ function element(type, prnt, atts, evts) {
   return elm
 }
 
-function setValue(name,value){
-  //alert(name+value)
-  name==='type'
-    ?setMainClass(value)
-    :setCSSProp(
-      name
-      ,['clr','bclr','part'].includes(name)
-        ?value
-        :easeInCirc(easeInCirc(value))
-    )
-}
-
-function setMainClass(value){
-  main.setAttribute('class', 'moire '+value)
-}
-
 function setCSSProp(name, value) {
   mainStyle.setProperty(`--${name}`, value)
 }
 
-function easeInCirc(x) {
-  return 1 - Math.sqrt(1 - Math.pow(x, 2))
-}
-
-function setData(){
-  const object = {}
-  const formData = new FormData(form)
-  formData.forEach((value, key) => object[key] = value)
-  location.hash = JSON.stringify(object)
-}
-
-function tryParse(s,data={}){
-  try {
-    data = JSON.parse(s)
-  } catch(err){}
-  return data
+function getRandomColor(){
+  return '#'+('00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6)
 }
 
 ////////
 
-export default experiment('moire', {init, exit}).expose
-
-function getStyle(){
-  return `
-:root {
-  --start: 0.4deg;
-  --end: 2deg;
-  --clr: white;
-  --bclr: black;
-  --t: 0;
-  --sin: 0;
-  --cos: 1;
-  --x: 1;
-  --y: 1;
-  --part: 0.1;
-  --total: 1;
-  --min: min(100vw,100vh);
-  --max: max(100vw,100vh);
-}
-
-.rotate {
-    width: 100%;
-    height: 100%;
-    padding: min(3vw, 3vh);
-    background-size: 100% 100%;
-    background-image: repeating-linear-gradient(calc(-0.1*var(--t)), black 0 4rem, gold 4rem 8rem);
-}
-
-.rotate div {
-    padding: inherit;
-    box-shadow: 0 0 2rem black;
-    /*backdrop-filter: blur(0.2vw);*/
-    background-image: inherit;
-    transform: rotate(calc(0.01*var(--t)));
-    transform-origin: 50% 50%;
-    /*border-radius: 998rem;*/
-}
-
-
-  `
-}
+export default experiment('rotate', {init, exit}).expose
