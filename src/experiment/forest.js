082cd810ed049b326e6fe4f55aeccb0e965dd1dc
Ron Valstar <ron@ronvalstar.nl>
2023-07-03 21:25:24 +0200

    Default forest

--- src/experiment/forest.js
+++ src/experiment/forest.js
@@ -20,31 +20,101 @@ function init(_target){
      viewBox="0 0 128 32"
      preserveAspectRatio="xMidYMax slice"
 >
-  <use xlink:href="#tree" style="--scale: 0.300; animation-delay: 164.993ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.301; animation-delay: 883.396ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.305; animation-delay: 213.447ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.312; animation-delay: 772.055ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.321; animation-delay: 408.134ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.333; animation-delay: 928.155ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.348; animation-delay: 90.985ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.365; animation-delay: 136.102ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.385; animation-delay: 571.913ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.407; animation-delay: 642.578ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.432; animation-delay: 266.957ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.460; animation-delay: 862.047ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.491; animation-delay: 439.998ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.524; animation-delay: 905.851ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.559; animation-delay: 720.294ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.598; animation-delay: 247.333ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.639; animation-delay: 776.316ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.682; animation-delay: 911.239ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.729; animation-delay: 135.360ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.778; animation-delay: 942.932ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.829; animation-delay: 59.707ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.884; animation-delay: 268.505ms;"></use>
-  <use xlink:href="#tree" style="--scale: 0.940; animation-delay: 988.430ms;">
-      <use xlink:href="#tree" style="transform: translate(1px,10px) rotate(45deg); fill: red;"></use>
-  </use>
+
+      <g style="--scale: 0.300; animation-delay: 52.472ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-15px) rotate(-25deg) scale(0.13,0.13); "></use></g>
+      <g style="--scale: 0.300; animation-delay: 447.117ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-15px) rotate(-14deg) scale(0.12,0.11); "></use></g>
+      <g style="--scale: 0.301; animation-delay: 447.554ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-20px) rotate(-34deg) scale(0.25,0.37); "></use></g>
+      <g style="--scale: 0.303; animation-delay: 399.621ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-12px) rotate(-21deg) scale(0.20,0.41); "></use></g>
+      <g style="--scale: 0.305; animation-delay: 342.137ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-24px) rotate(-17deg) scale(0.14,0.33); "></use></g>
+      <g style="--scale: 0.308; animation-delay: 996.863ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-14px) rotate(34deg) scale(0.27,0.04); "></use></g>
+      <g style="--scale: 0.311; animation-delay: 950.817ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-12px) rotate(38deg) scale(0.38,0.45); "></use></g>
+      <g style="--scale: 0.316; animation-delay: 811.160ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-21px) rotate(-2deg) scale(0.24,0.41); "></use></g>
+      <g style="--scale: 0.320; animation-delay: 781.516ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-13px) rotate(16deg) scale(0.26,0.48); "></use></g>
+      <g style="--scale: 0.326; animation-delay: 521.494ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-16px) rotate(34deg) scale(0.17,0.31); "></use></g>
+      <g style="--scale: 0.332; animation-delay: 371.095ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-19px) rotate(-28deg) scale(0.25,0.46); "></use></g>
+      <g style="--scale: 0.338; animation-delay: 923.721ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-18px) rotate(47deg) scale(0.23,0.15); "></use></g>
+      <g style="--scale: 0.346; animation-delay: 703.301ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-7px) rotate(-15deg) scale(0.32,0.30); "></use></g>
+      <g style="--scale: 0.354; animation-delay: 81.474ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-17px) rotate(-16deg) scale(0.20,0.04); "></use></g>
+      <g style="--scale: 0.362; animation-delay: 839.978ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-24px) rotate(34deg) scale(0.17,0.35); "></use></g>
+      <g style="--scale: 0.371; animation-delay: 832.038ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-8px) rotate(-23deg) scale(0.10,0.00); "></use></g>
+      <g style="--scale: 0.381; animation-delay: 581.644ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-25px) rotate(-49deg) scale(0.39,0.30); "></use></g>
+      <g style="--scale: 0.392; animation-delay: 909.153ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-17px) rotate(-33deg) scale(0.17,0.07); "></use></g>
+      <g style="--scale: 0.403; animation-delay: 109.836ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-20px) rotate(-15deg) scale(0.39,0.17); "></use></g>
+      <g style="--scale: 0.414; animation-delay: 470.965ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-20px) rotate(-33deg) scale(0.35,0.06); "></use></g>
+      <g style="--scale: 0.427; animation-delay: 366.967ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-17px) rotate(22deg) scale(0.14,0.25); "></use></g>
+      <g style="--scale: 0.440; animation-delay: 372.970ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-26px) rotate(12deg) scale(0.39,0.05); "></use></g>
+      <g style="--scale: 0.453; animation-delay: 734.193ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-13px) rotate(42deg) scale(0.16,0.33); "></use></g>
+      <g style="--scale: 0.468; animation-delay: 754.851ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-23px) rotate(-42deg) scale(0.14,0.49); "></use></g>
+      <g style="--scale: 0.483; animation-delay: 670.514ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-9px) rotate(-36deg) scale(0.10,0.26); "></use></g>
+      <g style="--scale: 0.498; animation-delay: 396.674ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-14px) rotate(29deg) scale(0.21,0.06); "></use></g>
+      <g style="--scale: 0.514; animation-delay: 324.104ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-6px) rotate(26deg) scale(0.25,0.25); "></use></g>
+      <g style="--scale: 0.531; animation-delay: 834.586ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-22px) rotate(35deg) scale(0.15,0.15); "></use></g>
+      <g style="--scale: 0.548; animation-delay: 108.131ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-25px) rotate(-39deg) scale(0.37,0.46); "></use></g>
+      <g style="--scale: 0.567; animation-delay: 861.982ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-15px) rotate(-12deg) scale(0.27,0.37); "></use></g>
+      <g style="--scale: 0.585; animation-delay: 466.721ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-7px) rotate(22deg) scale(0.31,0.04); "></use></g>
+      <g style="--scale: 0.605; animation-delay: 402.287ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-7px) rotate(42deg) scale(0.29,0.02); "></use></g>
+      <g style="--scale: 0.624; animation-delay: 465.376ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-25px) rotate(-35deg) scale(0.38,0.19); "></use></g>
+      <g style="--scale: 0.645; animation-delay: 61.905ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-22px) rotate(-48deg) scale(0.32,0.33); "></use></g>
+      <g style="--scale: 0.666; animation-delay: 303.155ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-13px) rotate(24deg) scale(0.22,0.45); "></use></g>
+      <g style="--scale: 0.688; animation-delay: 472.950ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-24px) rotate(9deg) scale(0.13,0.49); "></use></g>
+      <g style="--scale: 0.711; animation-delay: 656.645ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-18px) rotate(22deg) scale(0.27,0.33); "></use></g>
+      <g style="--scale: 0.734; animation-delay: 58.655ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-13px) rotate(-43deg) scale(0.23,0.22); "></use></g>
+      <g style="--scale: 0.758; animation-delay: 394.872ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-12px) rotate(-15deg) scale(0.21,0.27); "></use></g>
+      <g style="--scale: 0.782; animation-delay: 723.744ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-18px) rotate(-17deg) scale(0.25,0.09); "></use></g>
+      <g style="--scale: 0.807; animation-delay: 396.143ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-13px) rotate(6deg) scale(0.18,0.29); "></use></g>
+      <g style="--scale: 0.833; animation-delay: 381.533ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-21px) rotate(40deg) scale(0.12,0.25); "></use></g>
+      <g style="--scale: 0.859; animation-delay: 860.080ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-21px) rotate(8deg) scale(0.26,0.40); "></use></g>
+      <g style="--scale: 0.886; animation-delay: 84.208ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-23px) rotate(3deg) scale(0.24,0.35); "></use></g>
+      <g style="--scale: 0.913; animation-delay: 737.173ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-12px) rotate(13deg) scale(0.40,0.21); "></use></g>
+      <g style="--scale: 0.942; animation-delay: 103.684ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-20px) rotate(22deg) scale(0.23,0.09); "></use></g>
+      <g style="--scale: 0.971; animation-delay: 901.239ms;"><use xlink:href="#tree"></use>
+        <use xlink:href="#tree" style=" transform-origin: center bottom; transform: translate(0,-9px) rotate(-27deg) scale(0.34,0.25); "></use>
 
   <style>
     .svg-trees {
@@ -78,38 +148,36 @@ function init(_target){
   </defs>
 </svg>`
   )
-
-  const svg = document.querySelector('.svg-trees')
-  Array.from(svg.querySelectorAll('use')).forEach(use=>use.remove())
-  const NSSVG = 'http://www.w3.org/2000/svg'
-  const NSXLINK = 'http://www.w3.org/1999/xlink'
-  const fragment = document.createDocumentFragment()
-  Array.from(new Array(47)).map((o,i,a)=>{
-    const scale = (0.3 + 0.7*Math.pow(i/a.length, 2)).toFixed(3)
-    const delay = (Math.random()*999).toFixed(3)
-
-    const g = document.createElementNS(NSSVG, 'g')
-    g.setAttribute('style', `--scale: ${scale}; animation-delay: ${delay}ms;`)
-
-    const use = document.createElementNS(NSSVG, 'use')
-    use.setAttributeNS(NSXLINK, 'href', '#tree')
-    g.appendChild(use)
-
-    const branch = document.createElementNS(NSSVG, 'use')
-    branch.setAttributeNS(NSXLINK, 'href', '#tree')
-    branch.setAttribute('style', `
-      transform-origin: center bottom;
-      transform:
-          translate(0,-${6+20*Math.random()}px)
-          rotate(${-50+100*Math.random()}deg)
-          scale(${0.1+0.3*Math.random()},${0.5*Math.random()});
-    `)
-    g.appendChild(branch)
-
-    fragment.appendChild(g)
-  })
-  svg.appendChild(fragment)
-
+  // const svg = document.querySelector('.svg-trees')
+  // Array.from(svg.querySelectorAll('use')).forEach(use=>use.remove())
+  // const NSSVG = 'http://www.w3.org/2000/svg'
+  // const NSXLINK = 'http://www.w3.org/1999/xlink'
+  // const fragment = document.createDocumentFragment()
+  // Array.from(new Array(47)).map((o,i,a)=>{
+  //   const scale = (0.3 + 0.7*Math.pow(i/a.length, 2)).toFixed(3)
+  //   const delay = (Math.random()*999).toFixed(3)
+  //
+  //   const g = document.createElementNS(NSSVG, 'g')
+  //   g.setAttribute('style', `--scale: ${scale}; animation-delay: ${delay}ms;`)
+  //
+  //   const use = document.createElementNS(NSSVG, 'use')
+  //   use.setAttributeNS(NSXLINK, 'href', '#tree')
+  //   g.appendChild(use)
+  //
+  //   const branch = document.createElementNS(NSSVG, 'use')
+  //   branch.setAttributeNS(NSXLINK, 'href', '#tree')
+  //   branch.setAttribute('style', `
+  //     transform-origin: center bottom;
+  //     transform:
+  //         translate(0,-${(6+20*Math.random()).toFixed(0)}px)
+  //         rotate(${(-50+100*Math.random()).toFixed(0)}deg)
+  //         scale(${(0.1+0.3*Math.random()).toFixed(2)},${(0.5*Math.random()).toFixed(2)});
+  //   `)
+  //   g.appendChild(branch)
+  //
+  //   fragment.appendChild(g)
+  // })
+  // svg.appendChild(fragment)
   zuper.init(_target)
 }
 
